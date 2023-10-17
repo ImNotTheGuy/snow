e08a2cb67ec0bfaf9ee2a3842092d3a7ffd1a0de
ImNotTheGuy <lraulin843@gmail.com>
2023-10-17 20:59:34 +0200

    feat(rest): Added simple health controller

--- backend/snow/src/main/java/com/ludo/snow/rest/health/HealthController.java
+++ backend/snow/src/main/java/com/ludo/snow/rest/health/HealthController.java
@@ -1,2 +1,15 @@
-package com.ludo.snow.rest.health;public class HealthController {
+package com.ludo.snow.rest.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping()
+    public String health(){
+        return "Snow is up and running !";
+    }
 }
