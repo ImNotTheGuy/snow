ee247896a29c21aba79d2d3f197e68a0755c8635
ImNotTheGuy <lraulin843@gmail.com>
2023-10-17 22:10:45 +0200

    feat(rest): added Sl4hj on healthController

--- backend/snow/src/main/java/com/ludo/snow/rest/health/HealthController.java
+++ backend/snow/src/main/java/com/ludo/snow/rest/health/HealthController.java
@@ -1,15 +1,18 @@
 package com.ludo.snow.rest.health;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/health")
+@Slf4j
 public class HealthController {
 
     @GetMapping()
     public String health(){
+        log.info("App health check was requested");
         return "Snow is up and running !";
     }
 }
