1c1e84d33119379a6bc03ed4298fd535dac6c889
Ludo <lraulin843@gmail.com>
2023-11-12 18:26:37 +0100

    feat(docker): up and running docker-compose

--- backend/snow/src/main/java/com/ludo/snow/rest/health/HealthController.java
+++ backend/snow/src/main/java/com/ludo/snow/rest/health/HealthController.java
@@ -1,6 +1,8 @@
 package com.ludo.snow.rest.health;
 
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/health")
 @Slf4j
+@CrossOrigin
 public class HealthController {
 
     @GetMapping()
